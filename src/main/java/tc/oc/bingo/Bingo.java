9ccf9c2cf2f1d37b8bf207d62cb7bf8782bcbace
Pugzy <pugzy@mail.com>
2024-05-20 20:23:32 +0100

    Add hotswap objective tracker syncing
    Signed-off-by: Pugzy <pugzy@mail.com>

--- src/main/java/tc/oc/bingo/Bingo.java
+++ src/main/java/tc/oc/bingo/Bingo.java
@@ -2,11 +2,15 @@ package tc.oc.bingo;
 
 import co.aikar.commands.BukkitCommandManager;
 import fr.minuskube.inv.InventoryManager;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import lombok.AccessLevel;
@@ -23,12 +27,14 @@ import tc.oc.bingo.config.Config;
 import tc.oc.bingo.database.BingoCard;
 import tc.oc.bingo.database.BingoDatabase;
 import tc.oc.bingo.database.BingoPlayerCard;
+import tc.oc.bingo.database.ObjectiveItem;
 import tc.oc.bingo.listeners.PlayerJoinListener;
 import tc.oc.bingo.objectives.ObjectiveTracker;
 import tc.oc.bingo.objectives.Tracker;
 import tc.oc.bingo.util.Exceptions;
 import tc.oc.bingo.util.ManagedListener;
 import tc.oc.bingo.util.Reflections;
+import tc.oc.pgm.api.PGM;
 
 @Getter
 public class Bingo extends JavaPlugin {
@@ -36,7 +42,7 @@ public class Bingo extends JavaPlugin {
   private static Bingo INSTANCE;
 
   private BukkitCommandManager commands;
-  private List<ObjectiveTracker> trackers;
+  private final List<ObjectiveTracker> trackers = new ArrayList<>(25);
 
   @Getter(AccessLevel.NONE)
   private BingoDatabase database;
@@ -63,25 +69,6 @@ public class Bingo extends JavaPlugin {
     this.database = BingoDatabase.build(Config.get().getDatabase());
 
     this.cardRefresher = new CardRefresher();
-
-    trackers =
-        Reflections.findClasses(
-                ObjectiveTracker.class.getPackage().getName(),
-                ObjectiveTracker.class,
-                Tracker.class)
-            .stream()
-            .map(this::buildTracker)
-            .collect(Collectors.toList());
-
-    loadTrackerConfigs(getConfig());
-
-    // TODO: this
-    //  Load trackers based on if they're enabled or not (global config and existing on card (query
-    // first)
-    //  Don't recreate if they already exist
-    //  Remove ones that are no longer needed etc
-
-    getTrackersOfType(ManagedListener.class).forEach(ManagedListener::enable);
 
     Bukkit.getServer().getPluginManager().registerEvents(new PlayerJoinListener(this), this);
     this.rewards = new RewardManager(this);
@@ -134,10 +121,68 @@ public class Bingo extends JavaPlugin {
 
   public CompletableFuture<BingoCard> loadBingoCard() {
     return Exceptions.handle(
-        database.getCard().thenApply((bingoCard) -> this.bingoCard = bingoCard));
+        database
+            .getCard()
+            .thenApplyAsync(
+                (bingoCard) -> {
+                  resyncTrackers(bingoCard);
+                  return this.bingoCard = bingoCard;
+                },
+                PGM.get().getExecutor()));
   }
 
   public BingoDatabase getBingoDatabase() {
     return database;
+  }
+
+  private void resyncTrackers(BingoCard card) {
+    Set<String> objectivesToCreate =
+        card.getObjectives().stream()
+            .filter(Objects::nonNull)
+            .map(ObjectiveItem::getSlug)
+            .collect(Collectors.toSet());
+
+    Map<String, ObjectiveTracker> objectivesToRemove =
+        trackers.stream()
+            .collect(Collectors.toMap(ObjectiveTracker::getObjectiveSlug, Function.identity()));
+
+    objectivesToCreate.removeIf(string -> objectivesToRemove.remove(string) != null);
+
+    objectivesToRemove
+        .values()
+        .forEach(
+            tracker -> {
+              tracker.disable();
+              trackers.remove(tracker);
+            });
+
+    List<ObjectiveTracker> newTrackers = new ArrayList<>();
+    if (!objectivesToCreate.isEmpty()) {
+      // Find trackers that are not present in the current list and create
+      newTrackers =
+          Reflections.findClasses(
+                  ObjectiveTracker.class.getPackage().getName(),
+                  ObjectiveTracker.class,
+                  Tracker.class)
+              .stream()
+              .filter(
+                  trackerClass -> {
+                    Tracker annotation = trackerClass.getAnnotation(Tracker.class);
+                    return annotation != null && objectivesToCreate.contains(annotation.value());
+                  })
+              .map(this::buildTracker)
+              .collect(Collectors.toList());
+    }
+
+    newTrackers.forEach(
+        tracker -> {
+          ConfigurationSection section =
+              getConfig().getConfigurationSection(tracker.getObjectiveSlug());
+          if (section != null) tracker.setConfig(section);
+        });
+
+    newTrackers.forEach(ManagedListener::enable);
+
+    trackers.addAll(newTrackers);
   }
 }
