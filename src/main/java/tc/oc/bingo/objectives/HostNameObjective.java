680bef14c0425aa3916e761a69990ea3fdbdcd6f
Pablo Herrera <pabloherrerapalacio@gmail.com>
2025-04-05 00:11:42 +0200

    Fix lag-spikes from resolving hostname

--- src/main/java/tc/oc/bingo/objectives/HostNameObjective.java
+++ src/main/java/tc/oc/bingo/objectives/HostNameObjective.java
@@ -1,12 +1,12 @@
 package tc.oc.bingo.objectives;
 
+import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerLoginEvent;
-import tc.oc.bingo.Bingo;
+import tc.oc.pgm.api.PGM;
 
 @Tracker("hostname-check")
 public class HostNameObjective extends ObjectiveTracker {
@@ -16,11 +16,8 @@ public class HostNameObjective extends ObjectiveTracker {
   @EventHandler(priority = EventPriority.MONITOR)
   public void onPlayerLogin(PlayerLoginEvent event) {
     Player player = event.getPlayer();
-    String playerAddress = event.getAddress().getHostName().toLowerCase();
-
-    if (playerAddress.contains(REQUIRED_STRING.get())) {
-      Bukkit.getScheduler()
-          .runTaskLater(Bingo.get(), () -> reward(player), 100L); // 5 seconds delay
+    if (event.getHostname().toLowerCase().contains(REQUIRED_STRING.get())) {
+      PGM.get().getExecutor().schedule(() -> reward(player), 5L, TimeUnit.SECONDS);
     }
   }
 }
