a2cbc7979f685b9ffa388db4514fb34df6ea12dc
Pablo Herrera <pabloherrerapalacio@gmail.com>
2025-04-06 18:44:21 +0200

    Require ping & persist drink time

--- src/main/java/tc/oc/bingo/objectives/HydrationObjective.java
+++ src/main/java/tc/oc/bingo/objectives/HydrationObjective.java
@@ -1,7 +1,5 @@
 package tc.oc.bingo.objectives;
 
-import java.util.Map;
-import java.util.UUID;
 import java.util.function.Supplier;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -9,9 +7,10 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 @Tracker("stay-hydrated")
-public class HydrationObjective extends ObjectiveTracker.StatefulInt {
+public class HydrationObjective extends ObjectiveTracker.Stateful<HydrationObjective.DrinkStatus> {
 
   private final Supplier<Integer> MAX_SECONDS = useConfig("max-seconds", 1800);
   private final Supplier<Integer> MIN_SECONDS = useConfig("min-seconds", 300);
@@ -19,9 +18,6 @@ public class HydrationObjective extends ObjectiveTracker.StatefulInt {
   private final Supplier<Integer> REQUIRED_DRINKS = useConfig("required-drinks", 6);
   private final Supplier<Boolean> INCLUDE_POTIONS = useConfig("accept-potions", false);
 
-  private final Map<UUID, Long> lastDrink = useState(Scope.SESSION);
-
-  // TODO: also change to a record?
   // when a player consumes a water bottle log the millisecond of when this was completed.
   // Check this against the stored last drink value,
   // if the number of seconds is between the min and max since the last time then increment
@@ -40,22 +36,43 @@ public class HydrationObjective extends ObjectiveTracker.StatefulInt {
     if (item.getType() != Material.POTION) return;
     if (!INCLUDE_POTIONS.get() && item.getDurability() != 0) return;
 
+    var data = getObjectiveData(player.getUniqueId());
+
     long now = System.currentTimeMillis();
-    long drinkGap = now - lastDrink.getOrDefault(player.getUniqueId(), 0L);
+    long timeSinceDrink = now - data.lastDrank;
 
-    if (drinkGap < MIN_SECONDS.get() * 1000L) return;
-    lastDrink.put(player.getUniqueId(), now);
+    if (timeSinceDrink < MIN_SECONDS.get() * 1000L) return;
 
-    if (drinkGap > MAX_SECONDS.get() * 1000L) {
-      storeObjectiveData(player.getUniqueId(), 1);
-      return;
+    int newCount = (timeSinceDrink > MAX_SECONDS.get() * 1000L ? 0 : data.drinkCount) + 1;
+    storeObjectiveData(player.getUniqueId(), new DrinkStatus(newCount, now));
+
+    if (newCount >= REQUIRED_DRINKS.get()) {
+      reward(player);
     }
-
-    trackProgress(player);
   }
 
   @Override
-  protected int maxValue() {
-    return REQUIRED_DRINKS.get();
+  public @NotNull HydrationObjective.DrinkStatus initial() {
+    return new DrinkStatus(0, 0L);
   }
+
+  @Override
+  public @NotNull HydrationObjective.DrinkStatus deserialize(@NotNull String string) {
+    String[] vals = string.split(",");
+    // Backwards compat for pre-drink saving
+    return new DrinkStatus(
+        Integer.parseInt(vals[0]), vals.length > 1 ? Long.parseLong(vals[1]) : 0L);
+  }
+
+  @Override
+  public @NotNull String serialize(@NotNull DrinkStatus data) {
+    return data.drinkCount + "," + data.lastDrank;
+  }
+
+  @Override
+  public double progress(DrinkStatus data) {
+    return (double) data.drinkCount / REQUIRED_DRINKS.get();
+  }
+
+  public record DrinkStatus(int drinkCount, long lastDrank) {}
 }
--- src/main/java/tc/oc/bingo/objectives/PongObjective.java
+++ src/main/java/tc/oc/bingo/objectives/PongObjective.java
@@ -2,6 +2,9 @@ package tc.oc.bingo.objectives;
 
 import static net.kyori.adventure.text.Component.text;
 
+import java.util.Collections;
+import java.util.Set;
+import java.util.UUID;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,11 +14,12 @@ import tc.oc.pgm.api.player.MatchPlayer;
 @Tracker("ping-pong")
 public class PongObjective extends ObjectiveTracker {
 
+  private final Set<UUID> pinged = Collections.newSetFromMap(useState(Scope.LIFE));
+
   @EventHandler(priority = EventPriority.HIGHEST)
   public void onCommandPreprocess(PlayerCommandPreprocessEvent event) {
-
     String[] params = event.getMessage().split(" ");
-    if (params.length != 1) return;
+    if (params.length != 1 || params[0].length() != 5 || params[0].charAt(0) != '/') return;
 
     MatchPlayer player = getPlayer(event.getPlayer());
     if (player == null) return;
@@ -23,15 +27,14 @@ public class PongObjective extends ObjectiveTracker {
     if (params[0].equalsIgnoreCase("/ping")) {
       event.setCancelled(true);
       player.sendMessage(text("Pong!", NamedTextColor.YELLOW));
-    }
-
-    if (params[0].equalsIgnoreCase("/pong")) {
+      pinged.add(event.getPlayer().getUniqueId());
+    } else if (params[0].equalsIgnoreCase("/pong")) {
+      event.setCancelled(true);
       player.sendMessage(
           text(
               "I hear " + player.getNameLegacy() + " likes cute Asian boys.",
               NamedTextColor.YELLOW));
-      event.setCancelled(true);
-      reward(event.getPlayer());
+      if (pinged.contains(player.getId())) reward(event.getPlayer());
     }
   }
 }
